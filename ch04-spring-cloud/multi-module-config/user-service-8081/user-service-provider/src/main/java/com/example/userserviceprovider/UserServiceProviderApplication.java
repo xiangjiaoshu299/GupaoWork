f468cd50594ff76ad6041415c2410ad1585f0e09
DESKTOP-8S1BCU2\LENOVO <2099319236@qq.com>
2020-08-02 11:36:16 +0800

    加:user-service-provider

---
+++ ch04-spring-cloud/multi-module-config/user-service-8081/user-service-provider/src/main/java/com/example/userserviceprovider/UserServiceProviderApplication.java
@@ -0,0 +1,14 @@
+package com.example.userserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UserServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceProviderApplication.class, args);
+    }
+
+}
