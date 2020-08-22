547cb0cbc050fdf1daf12281a9f649c78d027ecd
DESKTOP-8S1BCU2\LENOVO <2099319236@qq.com>
2020-08-22 23:45:56 +0800

    加：配置中心手动刷新

--- ch04-spring-cloud/multi-module-config/user-service-8081/user-service-provider/src/main/java/com/example/userserviceprovider/DescController.java
+++ ch04-spring-cloud/multi-module-config/user-service-8081/user-service-provider/src/main/java/com/example/userserviceprovider/DescController.java
@@ -1,9 +1,11 @@
 package com.example.userserviceprovider;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope//允许刷新配置
 @RestController
 public class DescController {
 
