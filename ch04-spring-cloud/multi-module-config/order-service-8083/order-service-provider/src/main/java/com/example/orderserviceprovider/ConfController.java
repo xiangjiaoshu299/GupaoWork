333e3841979471965aaea8cc4f36d002702f60da
DESKTOP-8S1BCU2\LENOVO <2099319236@qq.com>
2020-08-25 22:06:34 +0800

    加：order-service也加配置中心

---
+++ ch04-spring-cloud/multi-module-config/order-service-8083/order-service-provider/src/main/java/com/example/orderserviceprovider/ConfController.java
@@ -0,0 +1,24 @@
+package com.example.orderserviceprovider;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope//允许刷新配置
+@RestController
+public class ConfController {
+
+    @Value("${conf-desc}")
+    private String desc;
+
+    //user-service.properties中的配置
+    @Value("${app-func}")
+    private String appFunc;
+
+    @GetMapping("getDesc")
+    public String getDesc(){
+
+        return "appFunction: " + appFunc + ", conf desc: " + desc;
+    }
+}
