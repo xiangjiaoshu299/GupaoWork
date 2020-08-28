7fca787165ec46817b1e57bc2765c503de3d2f2a
DESKTOP-8S1BCU2\LENOVO <2099319236@qq.com>
2020-08-29 06:49:42 +0800

    加：springboot的nacos-conf配置

---
+++ ch04-spring-cloud/springboot-nacos-consumer-demo/src/main/java/com/example/springbootnacosdemo/ConfController.java
@@ -0,0 +1,27 @@
+package com.example.springbootnacosdemo;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//提示：需要在nacos中新建一项配置，dataId设置springboot-nacos-demo，设置info=infoaaa，其它选项默认即可，格式可以用txt
+@NacosPropertySource(dataId = "springboot-nacos-consumer-demo", autoRefreshed = true)
+@RestController
+public class ConfController {
+
+    //spring-cloud-nacos中的方式：
+    //@Value("${value:not-find-conf-in-nacos}")
+    //private String info;
+
+    //springboot-nacos的方式
+    @NacosValue(value = "${info:not-find-conf-in-nacos}", autoRefreshed = true)
+    private String info;
+
+    @GetMapping("conf")
+    public String getConf() {
+
+        return info;
+    }
+}
