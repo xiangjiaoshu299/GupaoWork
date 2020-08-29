f2ecb83f9408c63b3a20acd8a070d91b7773b7ae
DESKTOP-8S1BCU2\LENOVO <2099319236@qq.com>
2020-08-29 19:21:07 +0800

    加：crud的作业

---
+++ ch04-spring-cloud/springcloud-nacos-demo-crud/springcloud-nacos-consumer/src/main/java/com/example/springcloudnacosconsumer/SpringcloudNacosConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.example.springcloudnacosconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class SpringcloudNacosConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudNacosConsumerApplication.class, args);
+    }
+
+}
---
+++ ch04-spring-cloud/springcloud-nacos-demo-crud/springcloud-nacos-provider/src/main/java/com/example/springcloudnacosprovider/SpringcloudNacosProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudnacosprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudNacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudNacosProviderApplication.class, args);
+    }
+
+}
